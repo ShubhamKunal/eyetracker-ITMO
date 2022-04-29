255c26031aa27829919fe87be516bb711927ff69
ShubhamKunal <shubhamkunal19@gmail.com>
2022-04-29 10:58:12 +0530

    Final draft

---
+++ main.py
@@ -0,0 +1,99 @@
+import cv2 as cv
+import numpy as np
+import module as m
+import time
+
+# Variables
+COUNTER = 0
+TOTAL_BLINKS = 0
+CLOSED_EYES_FRAME = 3
+cameraID = 0
+
+# variables for frame rate.
+FRAME_COUNTER = 0
+START_TIME = time.time()
+FPS = 0
+
+
+# creating camera object
+camera = cv.VideoCapture(0)
+# camera.set(3, 640)
+# camera.set(4, 480)
+
+while True:
+    FRAME_COUNTER += 1
+    # getting frame from camera
+    ret, frame = camera.read()
+    if ret == False:
+        break
+
+    # Converting frame into grayscale.
+    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    height, width = grayFrame.shape
+    circleCenter = (int(width/2), 50)
+    # calling the face detector funciton
+    image, face = m.faceDetector(frame, grayFrame)
+    if face is not None:
+
+        # calling landmarks detector funciton.
+        image, PointList = m.faceLandmakDetector(frame, grayFrame, face, False)
+        # print(PointList)
+
+        cv.putText(frame, f'FPS: {round(FPS,1)}',(460, 20), m.font, 0.7, m.YELLOW, 2)
+        RightEyePoint = PointList[36:42]
+        LeftEyePoint = PointList[42:48]
+        leftRatio, topMid, bottomMid = m.blinkDetector(LeftEyePoint)
+        rightRatio, rTop, rBottom = m.blinkDetector(RightEyePoint)
+        
+        # cv.circle(image, topMid, 2, m.YELLOW, -1)
+        # cv.circle(image, bottomMid, 2, m.YELLOW, -1)
+        # cv.circle(image, rTop, 2, m.YELLOW, -1)
+        # cv.circle(image, rBottom, 2, m.YELLOW, -1)
+
+        blinkRatio = (leftRatio + rightRatio)/2
+
+        if blinkRatio > 4:
+            COUNTER += 1
+            cv.putText(image, f'Blink', (300, 450), m.font, 0.8, m.LIGHT_BLUE, 2)
+        else:
+            if COUNTER > CLOSED_EYES_FRAME:
+                TOTAL_BLINKS += 1
+                COUNTER = 0
+        cv.putText(image, f'Total Blinks: {TOTAL_BLINKS}', (230, 17), m.font, 0.7, m.ORANGE, 2)
+
+        mask, pos, color = m.EyeTracking(frame, grayFrame, RightEyePoint)
+        maskleft, leftPos, leftColor = m.EyeTracking(frame, grayFrame, LeftEyePoint)
+
+        # draw background as line where we put text.
+        cv.line(image, (30, 90), (100, 90), color[0], 30)
+        cv.line(image, (25, 50), (135, 50), m.WHITE, 30)
+        cv.line(image, (int(width-150), 50), (int(width-45), 50), m.WHITE, 30)
+        cv.line(image, (int(width-140), 90), (int(width-60), 90), leftColor[0], 30)
+
+        # writing text on above line
+        cv.putText(image, f'{pos}', (35, 95), m.font, 0.6, color[1], 2)
+        cv.putText(image, f'{leftPos}', (int(width-140), 95), m.font, 0.6, leftColor[1], 2)
+        cv.putText(image, f'Right Eye', (35, 55), m.font, 0.6, color[1], 2)
+        cv.putText(image, f'Left Eye', (int(width-145), 55), m.font, 0.6, leftColor[1], 2)
+
+        # showing the frame on the screen
+        cv.imshow('ITMO eyetracker', image)
+    else:
+        cv.imshow('ITMO eyetracker', frame)
+
+    # calculating the seconds
+    SECONDS = time.time() - START_TIME
+    # calculating the frame rate
+    FPS = FRAME_COUNTER/SECONDS
+    
+    # defining the key to break the Loop
+    key = cv.waitKey(1)
+
+    # if q is pressed on keyboard: quit
+    if key == ord('q'):
+        break
+# closing the camera
+camera.release()
+
+# closing  all the windows
+cv.destroyAllWindows()
